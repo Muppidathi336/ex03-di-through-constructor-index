7ed49b847926839e3e1ccd25bb3f73e1023ee702
Muppidathi Boominathan <104143415+Muppidathi336@users.noreply.github.com>
2022-08-18 12:30:27 +0530

    Add files via upload

---
+++ ReadMe.txt
@@ -0,0 +1,5 @@
+Using XML based Dependency Injection
+
+Example 3 - XML injection - constructor using argument index
+
+DI using xml - in the xml, loading of bean by passing parameters values through index(for each parameters index, values can be passed)
---
+++ src/main/java/com/mb/ex03_di_through_constructor_index/ColoredShapeHolder.java
@@ -0,0 +1,44 @@
+package com.mb.ex03_di_through_constructor_index;
+
+public class ColoredShapeHolder {
+
+    private ColoredShape coloredShape = new ColoredShape();
+
+    // default count is 1.
+    private int quantity = 1;
+
+    public ColoredShapeHolder() {
+    }
+
+    public ColoredShapeHolder(final ColoredShape coloredShape, int quantity) {
+
+        this.coloredShape = coloredShape;
+        this.quantity = quantity;
+    }
+
+    public ColoredShape getColoredShape() {
+
+        return coloredShape;
+    }
+
+    public void setColoredShape(final ColoredShape coloredShape) {
+
+        this.coloredShape = coloredShape;
+    }
+
+    public int getQuantity() {
+
+        return quantity;
+    }
+
+    public void setQuantity(final int quantity) {
+
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+
+        return quantity + " count of colored shape: [ " + coloredShape + " ]";
+    }
+}
